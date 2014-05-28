81ba060bd94f003967908b05bf1ce768c4632c24
thehexia <onmyexia@live.com>
2014-05-28 18:14:10 -0400

    fixed commit because it didnt have all the files.

---
+++ pathos/Controllers/ChapterController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pathos.Models;
+
+namespace pathos.Controllers
+{ 
+    public class ChapterController : Controller
+    {
+        private ProjectsDBContext db = new ProjectsDBContext();
+
+        //
+        // GET: /Chapter/
+
+        public ViewResult Index()
+        {
+            var chapters = db.Chapters.Include(c => c.Project);
+            return View(chapters.ToList());
+        }
+
+        //
+        // GET: /Chapter/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Chapter chapter = db.Chapters.Find(id);
+            return View(chapter);
+        }
+
+        //
+        // GET: /Chapter/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Title");
+            return View();
+        } 
+
+        //
+        // POST: /Chapter/Create
+
+        [HttpPost]
+        public ActionResult Create(Chapter chapter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Chapters.Add(chapter);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Title", chapter.ProjectID);
+            return View(chapter);
+        }
+        
+        //
+        // GET: /Chapter/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Chapter chapter = db.Chapters.Find(id);
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Title", chapter.ProjectID);
+            return View(chapter);
+        }
+
+        //
+        // POST: /Chapter/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Chapter chapter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(chapter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Title", chapter.ProjectID);
+            return View(chapter);
+        }
+
+        //
+        // GET: /Chapter/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Chapter chapter = db.Chapters.Find(id);
+            return View(chapter);
+        }
+
+        //
+        // POST: /Chapter/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Chapter chapter = db.Chapters.Find(id);
+            db.Chapters.Remove(chapter);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ pathos/Models/ProjectsModels.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace pathos.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectID { get; set; }
+
+        [Required]
+        [StringLength(140, MinimumLength = 0, ErrorMessage = "You have a maximum of 140 characters for your title.")]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [StringLength(140, MinimumLength = 0, ErrorMessage = "You have a maximum of 140 characters.")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Chapter> Chapter { get; set; }
+    }
+
+    public class Chapter
+    {
+        [Key] 
+        public int ChapterID { get; set; }
+
+        [Required]
+        [StringLength(140, MinimumLength = 0, ErrorMessage = "You have a maximum of 140 characters for your title.")]
+        public string Title { get; set; }
+
+        [Required]
+        public int ProjectID { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [StringLength(140, MinimumLength = 0, ErrorMessage = "You have a maximum of 140 characters.")]
+        public string Description { get; set; }
+
+        public virtual Project Project { get; set; }
+    }
+
+    public class ProjectsDBContext : DbContext
+    {
+        public ProjectsDBContext() : base("ProjectsDBContext") 
+        {
+        }
+
+        public DbSet<Chapter> Chapters { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
