f04da13090dc4380628543b442024b337d417edf
thehexia <onmyexia@live.com>
2014-06-06 10:41:55 -0400

    Added StoreModel and Store Controller

---
+++ pathos/Controllers/StoreController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pathos.Models;
+
+namespace pathos.Controllers
+{ 
+    public class StoreController : Controller
+    {
+        private ProjectsDBContext db = new ProjectsDBContext();
+
+        public ActionResult Index() 
+        { 
+            return View(); 
+        }
+
+        public ActionResult BookCart()
+        {
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string query)
+        {
+            //search title for query
+
+            //search author name for query
+
+            //search description for query
+
+            return View();
+        }
+
+        public List<Project> SearchByTitle(string query)
+        {
+            return new List<Project>();
+        }
+
+        public List<Project> SearchByDescription(string query)
+        {
+            return new List<Project>();
+        }
+
+        public List<Project> SearchByAuthor(string query)
+        {
+            return new List<Project>();
+        }
+
+        //TODO: add genre to projects
+        public List<Project> SearchByGenre(string query)
+        {
+            return new List<Project>();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ pathos/Models/StoreModels.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Data.Entity;
+
+namespace pathos.Models
+{
+    public class Transaction
+    {
+
+        //default constructor
+        public Transaction()
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+                this.owner = HttpContext.Current.User.Identity.Name;
+            this.DateSold = DateTime.Now;
+        }
+
+        public Transaction(Chapter chapter)
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+                this.owner = HttpContext.Current.User.Identity.Name;
+
+            this.ChapterID = chapter.ChapterID;
+            this.price = chapter.Price;
+            this.DateSold = DateTime.Now;
+        }
+
+        [Key]
+        public int TransactionID { get; set; }
+
+        //person who made the transcaction
+        [Required]
+        public string owner { get; set; }
+
+        //content they purchased
+        [Required]
+        public int ChapterID { get; set; }
+
+        //price
+        [Required]
+        public decimal price { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateSold { get; set; }
+    }
+
+}
